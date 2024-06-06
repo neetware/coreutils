bfaeb44f24bc5a8d383047ef0d2d0814f2ccc206
anon <166017789+nushiredev@users.noreply.github.com>
2024-06-06 18:28:19 +0200

    Cloned "ls" command

---
+++ ls/main.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// enable FileManager class
+let fm = FileManager.default
+
+// get working directory
+let getWorkingDirectory = fm.currentDirectoryPath
+
+// get list of files
+let listFiles = try! fm.contentsOfDirectory(atPath: getWorkingDirectory)
+
+// print names of files
+for listFile in listFiles {
+    print(listFile)
+}
