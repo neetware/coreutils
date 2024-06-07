e5868c750daad0f459772fc670cf0585a836c3ef
anon <166017789+nushiredev@users.noreply.github.com>
2024-06-07 23:42:46 +0200

    added error handling

--- rm.swift
+++ rm.swift
@@ -4,11 +4,22 @@ import Foundation
 // enable FileManager
 let fm = FileManager.default
 
+// check if a commandline argument is present
+guard CommandLine.arguments.count > 1 else {
+    print("Usage: rm <filename>") // Print usage information
+    exit(EXIT_FAILURE)
+}
+
 // set commandline arguments
-let fileIn = CommandLine.arguments[1]
+let fileTarget = CommandLine.arguments[1]
 
 // delete file
-try fm.removeItem(atPath: fileIn)
+do {
+    try fm.removeItem(atPath: fileTarget)
+} catch {
+    print("Error: \(error.localizedDescription)")
+    exit(EXIT_FAILURE)
+}
 
 // print message and name of deleted file
-print("File deleted: \(fileIn)")
+print("File deleted: \(fileTarget)")
