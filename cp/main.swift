91aaf7847ba2fc6382eb49ce62944386cf9410e0
anon <166017789+nushiredev@users.noreply.github.com>
2024-06-07 21:11:17 +0200

    rewrote the "cp" command

---
+++ cp/main.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+let fm = FileManager.default
+
+// set commandline arguments
+let fileIn = CommandLine.arguments[1]
+let fileOut = CommandLine.arguments[2]
+
+// copy file
+try fm.copyItem(atPath: fileIn, toPath: fileOut)
+
+// print message (non-standard)
+print("File copied: \(fileIn) -> \(fileOut)")
